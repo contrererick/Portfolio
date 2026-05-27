ae0d7092f22b84424b8f5060600df61c4f35c86c
contrererick <erick.alejandro16@outlook.com>
2026-05-27 00:32:50 -0600

    feat: add static file serving and frontend entry point
    Added an entry point for static files from "public" dir. Changed the
    previous "/" route to "/health-check" for consistency with its purpose.
    Added the SPA fallback route and created index.html view.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-app.get('/', (req, res)=>{
+app.get('/health-check', (req, res)=>{
     res.send('App Works!');
+});
+
+app.get('/{*path}', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'));
 });
 
 export default app;
