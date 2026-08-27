22369a8b0323d267190dbb5fc2085161a1c5baca
contrererick <erick.alejandro16@outlook.com>
2026-08-27 11:05:49 -0600

    build: Implement Switching card script
    Implemented a projectsCarrousel function to switch between cards showing
    projects information when clicked.

--- src/public/scripts/projects.js
+++ src/public/scripts/projects.js
@@ -1,8 +1,9 @@
-function projectsCarrousel(projectsList, projectTitle, projectDescription) {
+function projectsCarrousel(projectsList, projectTitle, projectDescription,projectImage) {
     
     const list = document.querySelector(projectsList);
     const title = document.querySelector(projectTitle);
     const description = document.querySelector(projectDescription);
+    const image = document.querySelector(projectImage);
 
     list.addEventListener('click', (e) => {
         const target = e.target.closest('li');
@@ -10,10 +11,11 @@ function projectsCarrousel(projectsList, projectTitle, projectDescription) {
         
         title.textContent = target.textContent;
         description.textContent = target.dataset.description;
+        image.src = target.dataset.src;
 
         list.querySelectorAll('li').forEach(li => li.classList.remove('active'));
         target.classList.add('active');
     });
 }
 
-projectsCarrousel('#projects-list', '#project-title', '#project-description');
+projectsCarrousel('#projects-list', '#project-title', '#project-description', '#project-image');
